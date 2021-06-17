8cce69417f507445c6dac30f2dcddabaf97d0e5b
vlasek <vlasek.fv@gmail.com>
2021-06-17 11:31:48 +0200

    První funkční verze

--- app/src/main/java/com/upce/hangmanvb/Hangman.kt
+++ app/src/main/java/com/upce/hangmanvb/Hangman.kt
@@ -4,22 +4,25 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class Hangman(private val word: String) {
-    var lives: Int = 7
+    var lives: Int = 6
     var guessedArray = BooleanArray(word.length) { _ -> false }
 
-    fun render(imgView: ImageView, textView: TextView) {
+    fun render(imgView: ImageView?, textView: TextView) {
+        //TODO nakopírovat obrázky a nastavit metodu aby vykreslila příslušný dle počtu životů
         //Vygenerovat obrázek
-
+        //imgView.setImageResource()
         //Vygenerovat uhádnutá písmena
-        textView.setText(getTextForTextView())
+        textView.setText(getTextForTextView() + " - ${lives}")
     }
 
     fun guess(vararg letter: Char) {
         var letterGuessed = false
         for (i in word.indices) {
-            if (word[i].equals(letter)) {
-                guessedArray[i] = true
-                if (!letterGuessed) letterGuessed = true
+            for(j in letter.indices){
+                if (word[i].toLowerCase() == letter[j].toLowerCase()) {
+                    guessedArray[i] = true
+                    if (!letterGuessed) letterGuessed = true
+                }
             }
         }
         if (!letterGuessed) lives--
@@ -27,11 +30,11 @@ class Hangman(private val word: String) {
 
     private fun getTextForTextView(): String {
         var text = ""
-        for (i in 0..guessedArray.size) {
+        for (i in 0..guessedArray.size-1) {
             if (guessedArray[i]) {
                 text += word[i]
             } else {
-                text += "_"
+                text += "_ "
             }
         }
         return text
--- app/src/main/java/com/upce/hangmanvb/MainActivity.kt
+++ app/src/main/java/com/upce/hangmanvb/MainActivity.kt
@@ -2,10 +2,32 @@ package com.upce.hangmanvb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
 class MainActivity : AppCompatActivity() {
+
+    //TODO Reset hry
+    //TODO Skóre?
+    //TODO Uživatel může přidat slovo do databáze
+    //TODO Napojit na databázi (přidat do databáze slova z .txt)
+    val hangman:Hangman = Hangman("Analfabet")
+
+    //TODO Nakopírovat pro ostatní buttony + změnit písmena v guess
+    private val buttonA by lazy { findViewById<Button>(R.id.buttonA) }
+    private val textViewWord by lazy { findViewById<TextView>(R.id.textViewWord) }
+    private val imageViewHangman by lazy { findViewById<TextView>(R.id.imageViewHangman) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonA.setOnClickListener {
+            hangman.guess('a','á')
+            hangman.render(null,textViewWord)
+            buttonA.isEnabled = false
+            buttonA.background.alpha = 0
+        }
     }
-    var hangman:Hangman = Hangman(word = "Franta")
+
 }
